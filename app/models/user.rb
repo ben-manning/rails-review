abfccc81a830e772aaff4bc78e2fa1f624952e73
Ben Manning <bmanning@me.com>
2015-01-13 12:08:07 -0800

    favorite associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,4 +5,6 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
 
   has_many :articles, dependent: :destroy
+  has_many :favorites
+  has_many :favorite_articles, through: :favorites, source: :article 
 end
